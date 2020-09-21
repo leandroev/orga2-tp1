cb64c5a508e7d7de707d9d8b86b352e6e99ecf23
Leandro Vigali <lvigali@dc.uba.ar>
2020-09-21 20:17:32 -0300

    funciones floats + gitignore

--- solucion/lib.asm
+++ solucion/lib.asm
@@ -1,3 +1,5 @@
+extern malloc
+extern free 
 
 section .data
 
@@ -22,12 +24,49 @@ global treeInsert
 global treePrint
 
 ;*** Float ***
+				; int32_t floatCmp(float* a, float* b);
+floatCmp:		; eax <- int32_t  (rdi <- *a, rsi <- *b)
+	; si stackframe, no toco la pila, espero funcione
+	movss xmm0, [rdi] 	; segun el manual, los bits [127-32] se llenan de 0's
+	comiss xmm0, [rsi]	; comparo los floats
+	
+	je .sonIguales
+	ja .esMayor
+	jb .esMenor
+	
+	.sonIguales: 	
+		mov eax, 0
+		jmp .fin 
 
-floatCmp:
+	.esMayor:
+		mov eax, 1
+		jmp .fin 
+	
+	.esMenor:
+		mov eax, -1		; o es add rax, -1
+	
+	.fin:
+	ret
+				; float* floatClone(float* a);
+floatClone:		; rax <- *float    (rdi <- *a)
+	push rbp		; pila alieada
+	mov rbp, rsp
+	push rdi		; guardo rdi en la pila | pila desalineada
+	sub rsp, 8		; pila alineada
+
+	mov rdi, 4
+	call malloc 	; pido  bytes para el float
+
+	; recupero el puntero a float
+	add rsp,8 		
+	pop rdi			; rdi <- float *a
+	mov rsi, [rdi]	; obtengo el dato float apuntado
+	mov [rax], rsi	; lo copio en memoria
+	pop rbp
 ret
-floatClone:
-ret
+
 floatDelete:
+	jmp free
 ret
 
 ;*** String ***
